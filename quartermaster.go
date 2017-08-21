a8f4c264c6cd60b3f36fff1c01ca9a208cd67280
Steve Lacy <me@slacy.me>
2017-08-21 10:26:43 -0700

    gofmt

--- quartermaster.go
+++ quartermaster.go
@@ -6,19 +6,19 @@ import "fmt"
 import "github.com/stevelacy/quartermaster/manager"
 
 func main() {
-  fmt.Printf("Starting quartermaster \n")
-  tokenDescription :=  "Authentication token for cluster" +
-  "(or ENV TOKEN=<token>)"
-  token := flag.String("token", "", tokenDescription)
-  if *token == "" {
-    *token = os.Getenv("TOKEN")
-  }
-  flag.Parse()
+	fmt.Printf("Starting quartermaster \n")
+	tokenDescription := "Authentication token for cluster" +
+		"(or ENV TOKEN=<token>)"
+	token := flag.String("token", "", tokenDescription)
+	if *token == "" {
+		*token = os.Getenv("TOKEN")
+	}
+	flag.Parse()
 
-  if *token == "" {
-    fmt.Println("Error: Missing required parameters")
-    flag.PrintDefaults()
-    os.Exit(1)
-  }
-  manager.Init(*token, ":9090")
+	if *token == "" {
+		fmt.Println("Error: Missing required parameters")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	manager.Init(*token, ":9090")
 }
